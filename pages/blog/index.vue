b4d523eeabc61c41c1170a5a111171570c013af6
Ven109 <arturless109@gmail.com>
2024-07-11 18:59:53 +0200

    Test

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid grid-cols-3 gap-4 auto-rows-max auto-cols-max">
-        <ContentList :query="query">
+        <ContentList path="/blog">
             <template #default="{ list }">
                 <div v-for="blog in list" :key="blog._path">
                     <BlogCard :path="blog._path || ''" :title="blog.title || ''" :description="blog.description" />
