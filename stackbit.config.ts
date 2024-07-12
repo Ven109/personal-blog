d26bf47d1df743d9a3c614ff66a555a958d37c95
Ven109 <arturless109@gmail.com>
2024-07-12 11:51:28 +0200

    stackbit.config.ts aktualisieren

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,13 +2,15 @@ import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
+    cmsName: "contentful",
     "nodeVersion": "18",
-    "ssgName": "nuxt",
-    "experimental": {
-        "ssg": {
-            "logPatterns": {
-                up: ['success onPreInit'],
-            },
-        },
-    },
+    ssgName: "custom",
+    devCommand: "npm run dev -- -p {PORT}",
+    experimental: {
+        ssg: {
+          name: "Nuxt3",
+          logPatterns: { up: ["Local:    http"] },
+          passthrough: ["/vite-hmr/**"]
+        }
+    }
 });
