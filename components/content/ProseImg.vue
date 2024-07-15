832278a2c16b9d00eb21e7e9b765442086a671c3
Ven109 <arturless109@gmail.com>
2024-07-15 14:32:57 +0200

    Adds new content

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <img
+            :src="src"
+            class="h-full object-cover w-full"
+        >
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  src: string,
+}>();
+</script>
