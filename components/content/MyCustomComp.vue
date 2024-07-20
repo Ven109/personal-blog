046cd0301f3858248236f154e7060e5fe72425a4
Ven109 <arturless109@gmail.com>
2024-07-20 16:03:32 +0200

    Adds new component for testing

---
+++ components/content/MyCustomComp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{title}}
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: 'Hello World',
+    },
+});
+</script>
